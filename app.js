a9bc16f3397820349a755b796f790952f66353ce
John Garner <jdextergarneriv@gmail.com>
2020-11-05 13:24:34 -0500

    started on inquirer prompt and developed employee constructor

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your Managers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your Managers ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Managers Email?' 
+        },
+        {
+            type: 'input',
+            name: 'phone',
+            message: 'What is your Managers office number?'
+        }
+    ])
+}
+
+promptUser()
+.then(answers => {
+    console.log (answers);
+});
